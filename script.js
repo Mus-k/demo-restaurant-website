2d4b593b6c600332e3d94fcad141b25da1dea2e7
Musa Kamara <kamaramusa56@gmail.com>
2023-09-02 14:53:26 +0300

    added cancel button in the cart item

--- script.js
+++ script.js
@@ -13,27 +13,6 @@ document.querySelectorAll(".nav-links").forEach((n) =>
   })
 );
 
-// country with code
-// Wait for the DOM to load
-document.addEventListener("DOMContentLoaded", function () {
-  // Get the telephone input element
-  const input = document.querySelector("#telephone");
-
-  // Initialize the intl-tel-input plugin
-  //
-  const iti = window.intlTelInput(input, {
-    preferredCountries: ["tr", "ca", "gb", "au"], // Optionally, set the preferred countries
-    separateDialCode: true, // Show the dial code in a separate input
-    utilsScript:
-      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js", // Include the utils.js script
-  });
-
-  // Get the selected country dial code
-  iti.getSelectedCountryData().dialCode;
-});
-
-// form validation
-
 const contactForm = document.querySelector(".contact-form");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
